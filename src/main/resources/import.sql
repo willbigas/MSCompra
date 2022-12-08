8c04d7434b38c1455618b4c1f234d6ce61c20074
William Bigas Mauro <william.mauro@ibm.com>
2022-12-08 17:38:12 -0300

    add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO TB_PEDIDO (id, cep, cpf_cliente, data_compra, email, nome, produto, valor) VALUES (1, '56320000', '872.286.830-56', '2022-01-01', 'lucas.barros@hotmail.com', 'Lucas Barros', 1, 1000)
